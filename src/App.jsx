83b361a28a67df7ecffbf4653c555a6d149582b3
abutalha08 <taufique752@gmail.com>
2026-03-31 04:58:20 +0600

    Json file created and Premium  Courses section created and make this responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
 import './App.css'
 import Banner from './components/Banner/Banner'
+import Courses from './components/Courses/Courses'
 import Footer from './components/Footer/Footer'
 import GettingStarted from './components/GettingStarted/GettingStarted'
 import NavBar from './components/NavBar/NavBar'
 import StatsSection from './components/StatsSection/StatsSection'
 import TransparentPricing from './components/TransparentPricing/TransparentPricing'
 import Workflow from './components/Workflow/Workflow'
+
+
+const getCourses = async ()=> {
+  const response = await fetch("/course.json")
+  return response.json()
+}
+
+const coursesPromise = getCourses();
 
 function App() {
 
@@ -24,6 +33,8 @@ function App() {
       <Banner></Banner>
 
       <StatsSection></StatsSection>
+
+      <Courses coursesPromise = {coursesPromise}></Courses>
 
       <GettingStarted></GettingStarted>
       <TransparentPricing></TransparentPricing>
