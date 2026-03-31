ba550e8d1b3498490853b2258a80b25be07a860f
abutalha08 <taufique752@gmail.com>
2026-03-31 23:56:42 +0600

    Give Background color in tabs as per figma

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React from "react";
 
-const Tabs = ({ setActiveTab, carts}) => {
-    return (
-        <div>
-            {/* name of each tab group should be unique */}
-<div className="tabs tabs-box  justify-center bg-transparent">
-  <input type="radio" name="my_tabs_1" className="tab rounded-full w-40 font-bold " aria-label="Products" onClick={() => setActiveTab("product")} defaultChecked />
-  <input type="radio" name="my_tabs_1" className="tab rounded-full w-40 font-bold " aria-label= {`Cart (${carts.length})`} onClick={() => setActiveTab("cart")}  />
- 
-</div>
-        </div>
-    );
+const Tabs = ({ activeTab, setActiveTab, carts }) => {
+  return (
+    <div>
+      {/* name of each tab group should be unique */}
+      <div className="tabs tabs-box  justify-center bg-transparent">
+        <input
+          type="radio"
+          name="my_tabs_1"
+          className={`tab rounded-full w-40 font-bold
+                        ${activeTab === "product" ? "tab-active bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white" : ""}`}
+          aria-label="Products"
+          onClick={() => setActiveTab("product")}
+          defaultChecked
+        />
+
+        <input
+          type="radio"
+          name="my_tabs_1"
+          className={`tab rounded-full w-40 font-bold
+                        ${activeTab === "cart" ? "tab-active bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white" : ""}`}
+          aria-label={`Cart (${carts.length})`}
+          onClick={() => setActiveTab("cart")}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Tabs;
